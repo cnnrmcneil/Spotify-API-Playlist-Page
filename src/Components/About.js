95c795f5c9c6a54472cd16bf9d28de507cebb4ce
Connor McNeil <cnnrmcnl@gmail.com>
2021-12-22 18:54:02 -0500

    about page updated

--- src/Components/About.js
+++ src/Components/About.js
@@ -3,27 +3,35 @@ import React from "react";
 const About = () => {
   return (
     <div className="about">
-      <h1>SpotList: by Connor McNeil</h1>
-      <h2>Accessing the the home page: </h2>
+      <h1>SpotList by Connor McNeil</h1>
+      <h2>Accessing the the home page</h2>
       <p>
         If you would like to view the rest of the site, please email me your
         Spotify email & display name at cnnrmcnl@gmail.com so that the Spotify
         API can provide you an approved login token.
       </p>
       <h2>How the website functions</h2>
+      <img
+        src="https://i.ibb.co/YRH1Vw6/spotify-login-page.png"
+        className="image1"
+      />
       <p>
-        Upon pressing the Sign In to Spotify button, Spotify will notify you of
-        the permissions being requested and provide you with a token to make
-        requests to the Spotify API
+        Upon pressing the Sign In to Spotify button, Spotify will notify the
+        user of the permissions being requested and provide the user with a
+        token to make requests to the Spotify API
       </p>
+      <img
+        src="https://i.ibb.co/BK4ndfY/allplaylistsandsongs.png"
+        className="image2"
+      />
       <p>
-        On the home page, Spotify will pull your information from your public
-        playlists and display them. The site also features a Web Player that
-        allows you to manage your songs. Context manages what playlist and song
-        information, as well as storing your token that is used for API
-        requests.
+        On the home page, Spotify will pull the users information from your
+        public playlists and display them. The site also features a Web Player
+        that allows users to manage their songs. Context manages the playlists
+        and active song information, as well as storing the users token that is
+        used for API requests.
       </p>
-      <p>
+      <p className="lastP">
         Thanks to{" "}
         <a href="https://github.com/gilbarbara/react-spotify-web-playback">
           Gil Barbara
