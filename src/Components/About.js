64019335ff929e6b7f0a744d53a5957e02a2a50c
Connor McNeil <cnnrmcnl@gmail.com>
2021-12-22 18:09:37 -0500

    about page updates

--- src/Components/About.js
+++ src/Components/About.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const About = () => {
   return (
-    <div>
+    <div className="about">
       <h1>SpotList: by Connor McNeil</h1>
       <h2>Accessing the the home page: </h2>
       <p>
@@ -15,6 +15,24 @@ const About = () => {
         Upon pressing the Sign In to Spotify button, Spotify will notify you of
         the permissions being requested and provide you with a token to make
         requests to the Spotify API
+      </p>
+      <p>
+        On the home page, Spotify will pull your information from your public
+        playlists and display them. The site also features a Web Player that
+        allows you to manage your songs. Context manages what playlist and song
+        information, as well as storing your token that is used for API
+        requests.
+      </p>
+      <p>
+        Thanks to{" "}
+        <a href="https://github.com/gilbarbara/react-spotify-web-playback">
+          Gil Barbara
+        </a>{" "}
+        for the Spotify Web Player and{" "}
+        <a href="https://github.com/thelinmichael/spotify-web-api-node">
+          Michael Thelin
+        </a>{" "}
+        for the Spotify Web API Node
       </p>
     </div>
   );
